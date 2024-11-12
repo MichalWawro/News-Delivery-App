0a69bf18ed5678f62f562bae91fafe19f81c67b6
Michał Wawro <mwawro77@gmail.com>
2024-11-13 00:08:32 +0100

    Added my EC2 public IP to CORS .allowedOrigins

--- server/src/main/java/com/example/newsdelive/config/WebConfig.java
+++ server/src/main/java/com/example/newsdelive/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://98.85.16.27:3000"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
